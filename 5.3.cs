dd6678ad2ddbfba27f34364898425f592c608cef
shutnek <39194745+shutnek@users.noreply.github.com>
2021-10-21 15:24:38 +0300

    Update 5.3.cs

--- 5.3.cs
+++ 5.3.cs
@@ -7,7 +7,7 @@ namespace _5._3
         static void Main(string[] args)
         {
             double a, b, c, l, l2 = 0;
-            Console.WriteLine("введите значение 3 точек на числовой оси(A B C)");
+            Console.WriteLine("введите значение 3 точек на числовой оси(A B C) (точка C между A и B )");
             a = Convert.ToDouble(Console.ReadLine());
             b = Convert.ToDouble(Console.ReadLine());
             c = Convert.ToDouble(Console.ReadLine());
