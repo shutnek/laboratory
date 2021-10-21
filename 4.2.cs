c0b0fd8613a9b07346468779414efd0cdc2ad8b8
shutnek <39194745+shutnek@users.noreply.github.com>
2021-10-21 14:36:45 +0300

    Update 4.2.cs

--- 4.2.cs
+++ 4.2.cs
@@ -6,14 +6,13 @@ namespace номер3
     {
         static void Main(string[] args)
         {
-            double a = 0;
-            double b = 0;
+            double d = 0;
+            double p = 3.14;
             double c = 0;
-            Console.WriteLine("Введите два числа ");
-            a = Convert.ToDouble(Console.ReadLine());
-            b = Convert.ToDouble(Console.ReadLine());
-            c = (a*b)/2;
-            Console.Write("Среднее арефмитическое= ");
+            Console.WriteLine("Введите диаметр ");
+            d = Convert.ToDouble(Console.ReadLine());
+            c = p*d;
+            Console.Write("Длина окружности ");
             Console.Write(c); 
         }
     }
