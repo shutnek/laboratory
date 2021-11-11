185b8c9e9881341d47ce99931709d19968fc6708
shutnek <39194745+shutnek@users.noreply.github.com>
2021-11-11 15:22:05 +0300

    Update 8,1.cs

--- 8,1.cs
+++ 8,1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp9
 {
@@ -6,15 +6,26 @@ namespace ConsoleApp9
     {
         static void Main(string[] args)
         {
-            double a = 0;
-            Console.WriteLine("введите размер файла в байтах ");
-            a = Convert.ToDouble(Console.ReadLine());
+            int a,b,c,d = 0;
+            int l =1024;
+            Console.WriteLine("введите размер файла в байтах(целое число) ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b=a % 1024;
+if(b==0){
+    c = a / l;
+    
+}
+    else{
+       d = a / l; 
+       c=d + 1;
+        
+    }
 
-            a = a / 1024;
+            
 
 
-            Console.Write(a);
-            Console.WriteLine("килобайт");
+            Console.Write(c);
+            Console.WriteLine(" килобайт");
         }
     }
 }
