3f7d461ae8ac63a0349cbfb106a532b2adc6247f
shutnek <39194745+shutnek@users.noreply.github.com>
2021-11-25 15:29:24 +0300

    Update 11,3.cs

--- 11,3.cs
+++ 11,3.cs
@@ -6,20 +6,31 @@ namespace ConsoleApp30
     {
         static void Main(string[] args)
         {
-            double a, b, c, d, l = 0;
-            Console.WriteLine("Введите  три целых числа");
-            a = Convert.ToDouble(Console.ReadLine());
-            b = Convert.ToDouble(Console.ReadLine());
-            c = Convert.ToDouble(Console.ReadLine());
-            if (Math.Abs(a - b) < Math.Abs(a - c))
+            double x1, x2, x3, y1,y2,y3, l1,l2 = 0;
+            Console.WriteLine("Введите  кординаты точки  A B  c");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            y1 = Convert.ToDouble(Console.ReadLine());
+            x2 = Convert.ToDouble(Console.ReadLine());
+             y2 = Convert.ToDouble(Console.ReadLine());
+            x3 = Convert.ToDouble(Console.ReadLine());
+             y3 = Convert.ToDouble(Console.ReadLine());
+            l1=Math.Sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)));
+            l2=Math.Sqrt(((x1-x3)*(x1-x3))+((y1-y3)*(y1-y3)));
+            if (l1 < l2)
             {
                 Console.Write("ближе точка b на растоянии ");
-                Console.WriteLine(Math.Abs(a - b));
+                Console.WriteLine(l2-l1);
             }
-            else
+            
+            if (l2 < l1)
             {
                 Console.Write("ближе точка c на растоянии ");
-                Console.WriteLine(Math.Abs(a - c));
+                Console.WriteLine(Math.Abs(l1 - l2));
+            }
+            if (l2 == l1)
+            {
+                Console.Write("на одинаковом растоянии");
+                Console.WriteLine(Math.Abs(l1 - l2));
             }
         }
     }
